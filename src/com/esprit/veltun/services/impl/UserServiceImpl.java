b37ab1a002822822e640a014cad66b6974d10f79
reziguialaa <alaa.rezigui@esprit.tn>
2023-02-12 17:30:08 +0100

    update userservice

--- src/com/esprit/veltun/services/impl/UserServiceImpl.java
+++ src/com/esprit/veltun/services/impl/UserServiceImpl.java
@@ -61,10 +61,10 @@ public class UserServiceImpl implements UserService {
 	}
 
 	@Override
-	public boolean remove(String id) {
+	public boolean remove(Integer id) {
 		try {
 		    Connection conn = MyConnection.getInstance();
-			String req = "DELETE FROM `user` WHERE CIN  LIKE " + id;
+			String req = "DELETE FROM `user` WHERE id = " + id;
 			Statement st = conn.createStatement();
 			st.executeUpdate(req);
 			System.out.println("Personne deleted !");
