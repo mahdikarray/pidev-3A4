8555f225f18ddea7e2b26136050cc7146de5f312
Rim HAMMAMI <rim.hammami@esprit.tn>
2023-02-12 13:15:41 +0100

    create BaseEntity class

---
+++ src/com/esprit/veltun/model/BaseEntity.java
@@ -0,0 +1,13 @@
+package com.esprit.veltun.model;
+
+public abstract class BaseEntity {
+	private Integer id;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+}
