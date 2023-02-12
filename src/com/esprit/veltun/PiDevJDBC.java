40890fd55555bfb2cfa6840801859344bd49e9cf
Rim HAMMAMI <rim.hammami@esprit.tn>
2023-02-12 11:49:43 +0100

    init

---
+++ src/com/esprit/veltun/PiDevJDBC.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.esprit.veltun;
+import java.sql.Connection;
+
+import com.esprit.veltun.model.Personne;
+import com.esprit.veltun.util.MyConnection;
+/**
+ *
+ * @author Asus
+ */
+public class PiDevJDBC {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Connection conn= MyConnection.getInstance();
+        
+        
+        
+        
+    }
+    
+}
