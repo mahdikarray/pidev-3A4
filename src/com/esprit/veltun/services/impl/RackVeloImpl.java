e2df95236b0789cfe0f12e4df65f7efabb66cf4f
rym-bouaicha <rim.bouaicha@esprit.tn>
2023-02-12 21:28:21 +0000

    update

---
+++ src/com/esprit/veltun/services/impl/RackVeloImpl.java
@@ -0,0 +1,40 @@
+package com.esprit.veltun.services.impl;
+import java.sql.Connection;
+import java.util.Collection;
+
+import com.esprit.veltun.model.RackVelo;
+import com.esprit.veltun.services.RackVeloService;
+
+public class RackVeloImpl implements RackVeloService{
+
+	@Override
+	public RackVelo findById(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Collection<RackVelo> list() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public RackVelo save(RackVelo entity) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public RackVelo update(RackVelo entity) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean remove(Integer id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
